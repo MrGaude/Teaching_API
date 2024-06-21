0ca960525cf0710f7646e256e441bcb62633126c
Michael Gaudet <Mr.Michael.Gaudet@gmail.com>
2024-06-21 14:31:33 -0500

    Added barebones svelte app

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		//Do api stuff here!
+	});
+</script>
+
+<div>
+	<h1>Cat Facts</h1>
+	<p>Welcome to the cat facts page</p>
+</div>
+
+<div>
+	<!-- Place cat photo and generic infomation below -->
+	<!-- <img src="" alt="cat" /> -->
+	<p>[cat fact]</p>
+</div>
---
+++ readme.md
@@ -0,0 +1,5 @@
+The point of this exercise is to pull data from the cat api and display it on the page.
+
+The api I'm using is https://thecatapi.com/
+
+you will pull the data from thecatapi, display, the image, and some information on the web page.
